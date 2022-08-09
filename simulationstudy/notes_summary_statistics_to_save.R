b008f99118873e05373ec28fdfbdd51d9f9b9b1a
julia dyck <julia.alexandra.dyck@gmail.com>
2022-08-09 16:11:20 +0200

    plan simulation output (started)

--- simulationstudy/notes_summary_statistics_to_save.R
+++ simulationstudy/notes_summary_statistics_to_save.R
@@ -4,27 +4,48 @@
 # for each repetition: save everything in one large vector containing:
 
 # datagenerating parameters (important, once different scenarios are compared)
-# ## ...
-# ## ...
-# ## seed before data generation
+# [, -] (data generating process)    # <- when only one alternative -> (...)
+# [, 1] sample size
+# [, 2] backround rate
+# [, 3] ADR rate
+# [, 4] mean of ADR event-time (randomly drawn)
+# [, -] (relative sd of ADR event-time)
+# [, 5] seed before data generation (can be used to merge outcomes for different models based on the same data)
 
 # for each model:
 
 # model form and prior values
-# ## stancode name
-# ## prior means and sds
+# [, 6] stancode name
+# [, 7] prior theta mean
+# [, 8] prior theta sd
+# [, 9] prior nu mean
+# [,10] prior nu sd
+# [,11] prior gamma mean
+# [,12] prior gamma sd
 
 # modelfitting specifications
-# ## sampler
-# ## number of chains
-# ## posterior sample size per chain
-# ## warmup per chain
+# [, -] (sampler/algorithm -> NUTS)
+# [, -] number of chains
+# [, -] posterior sample size per chain
+# [, -] model seed (automatically generated)
+# [, -] elapsed time
 
-# posterior statistics
-# n_eff for each parameter
-# Rhat for each parameter
-# min, 0.025, 0.05, ..., 0.95, 0.975, max - Quantiles for each parameter (to gen. ETIs)
-#
+# posterior statistics for nu
+# [, ..] nu.mean
+# [, ..] nu.sd
+# [, 13] nu.n_eff
+# [, 14] nu.Rhat
+# [, 15:34] nu.ETIs (nu.ETI.50.l, nu.ETI.50.u, nu.ETI.55.l, nu.ETI.55.u,..., nu.ETI.95.l, nu.ETI.95.u)
+# [, 35:54] nu.HDIs (nu.HDI.50.l, nu.HDI.50.u, nu.HDI.55.l, nu.HDI.55.u,..., nu.HDI.95.l, nu.HDI.95.u)
+# [, 55:154] nu.percentiles to estimate prob mass within ropes (nu.perc.0, nu.perc.1, ..., nu.perc.100)
+# [, 155] nu.n_eff for each parameter
+# [, 156] nu.Rhat for each parameter
+# [, x:y] nu.ETIs (nu.ETI.50.l, nu.ETI.50.u, nu.ETI.55.l, nu.ETI.55.u,..., nu.ETI.95.l, nu.ETI.95.u)
+# [, x:y] nu.HDIs (nu.HDI.50.l, nu.HDI.50.u, nu.HDI.55.l, nu.HDI.55.u,..., nu.HDI.95.l, nu.HDI.95.u)
+# [, x:y] nu.percentiles to estimate prob mass within ropes (nu.perc.0, nu.perc.1, ..., nu.perc.100)
+
+
+# posterior statistics for gamma
 
 
 

f06fcbc02a9c8b6fdea9ce3206cf4799d054dd2e
julia dyck <julia.alexandra.dyck@gmail.com>
2022-08-11 12:48:44 +0200

    outsource stan modelling in seperate function

--- R/fit.fgg_function.R
+++ R/fit.fgg_function.R
@@ -2,13 +2,19 @@
 #'
 #' The functions essentially just apply the \code{rstan::stan} command with an already prespecified .stan file.
 #' Within a simulation study, the function can be run in parallel on various prior meta-parameter specifications or datasets.
+#'
+#' WICHTIG: warmup (1000) und iter (15000) muss noch angepasst werden!
+#'
+#' @export
+#'
+
 
 fit.fgg = function(datstan){
   output = rstan::stan(
-    file = "simulationstudy/fix.gam.gam.stan",  # Stan program
+    file = "stanfiles/fix.gam.gam.stan",  # Stan program
     model_name = "fix.gam.gam.model", # model name
     data = datstan,    # named list of data
-    chains = 4,             # number of Markov chains
+    chains = 1,             # number of Markov chains
     warmup = 100,          # number of warmup iterations per chain
     iter = 150,            # total number of iterations per chain
     cores = getOption("mc.cores", 1L) - 1,              # number of cores (one per chain)
@@ -17,10 +23,25 @@ fit.fgg = function(datstan){
   return(output)
 }
 
+#test.fit = fit.fgg(standat)
+#test.fit
+
+
+#' fit prespecified pgw stan models
+#'
+#' The functions essentially just apply the \code{rstan::stan} command with an already prespecified .stan file.
+#' Within a simulation study, the function can be run in parallel on various prior meta-parameter specifications or datasets.
+#'
+#' WICHTIG: warmup (1000) und iter (15000) muss noch angepasst werden!
+#'
+#' @export
+#'
+
+
 
 fit.ggg = function(datstan){
   output = rstan::stan(
-  file = "simulationstudy/gam.gam.gam.stan",  # Stan program
+  file = "stanfiles/gam.gam.gam.stan",  # Stan program
   model_name = "gam.gam.gam.model", # model name
   data = datstan,    # named list of data
   chains = 4,             # number of Markov chains
@@ -32,12 +53,24 @@ fit.ggg = function(datstan){
   return(output)
 }
 
+#' fit prespecified pgw stan models
+#'
+#' The functions essentially just apply the \code{rstan::stan} command with an already prespecified .stan file.
+#' Within a simulation study, the function can be run in parallel on various prior meta-parameter specifications or datasets.
+#'
+#' WICHTIG: warmup (1000) und iter (15000) muss noch angepasst werden!
+#'
+#' @export
+#'
+
+
+
 fit.fll = function(datstan){
   output = rstan::stan(
-    file = "simulationstudy/fix.gam.gam.stan",  # Stan program
+    file = "stanfiles/fix.gam.gam.stan",  # Stan program
     model_name = "fix.log.log.model", # model name
     data = datstan,    # named list of data
-    chains = 4,             # number of Markov chains
+    chains = 1,             # number of Markov chains
     warmup = 100,          # number of warmup iterations per chain
     iter = 150,            # total number of iterations per chain
     cores = getOption("mc.cores", 1L) - 1,     # number of cores (one per chain)
@@ -46,12 +79,21 @@ fit.fll = function(datstan){
   return(output)
 }
 
+#' fit prespecified pgw stan models
+#'
+#' The functions essentially just apply the \code{rstan::stan} command with an already prespecified .stan file.
+#' Within a simulation study, the function can be run in parallel on various prior meta-parameter specifications or datasets.
+#'
+#' WICHTIG: warmup (1000) und iter (15000) muss noch angepasst werden!
+#'
+#' @export
+#'
 
-test.fit1 = fit.lll(datstan = standat)
+
 
 fit.lll = function(datstan){
   output = rstan::stan(
-    file = "simulationstudy/log.log.log.stan",  # Stan program
+    file = "stanfiles/log.log.log.stan",  # Stan program
     model_name = "log.log.log.model", # model name
     data = datstan,    # named list of data
     chains = 4,             # number of Markov chains
@@ -62,3 +104,4 @@ fit.lll = function(datstan){
   )
   return(output)
 }
+

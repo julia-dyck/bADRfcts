0ea817b0d6df1987943e6de4423ada8f3dc153ce
julia dyck <julia.alexandra.dyck@gmail.com>
2022-08-11 12:47:48 +0200

    done

--- simulationstudy/todos.R
+++
@@ -1,4 +0,0 @@
-# 1. write a fit.fgg.model fct with only one input argument (data)
-
-# 2. use it in sim.fit.to.one.sample with lapply -> so more cores can be used parallel
-#    -> all priors and pone model in parallel

826745bf34a6f729b9d112bfa21fee0e7a7745e2
julia dyck <julia.alexandra.dyck@gmail.com>
2022-09-13 11:59:13 +0200

    experimenting finished, documentation started

---
+++ R/stanfit.to.metastats.R
@@ -0,0 +1,54 @@
+#' extract meta  and prior statistics from stanfit object
+#'
+#' The function extracts iformation about the model fitting and prior specifications for the parameters to be estimated.
+#'
+#' @param stanfit.object the estimated stan model output
+#' @param stan.dat the data inserted for model fitting
+#'
+#'
+#' @return Information about the model fitting, such as:
+#' \item{model.seed}{the seed was generated and set to make the posterior samples reproducible}
+#' \item{stancode.name}{to identify the used model}
+#' \item{th.pr.mean}{prior mean of parameter theta}
+#' \item{th.pr.sd}{prior standard deviation of parameter theta}
+#' \item{nu.pr.mean}{prior mean of parameter nu}
+#' \item{nu.pr.sd}{prior standard deviation of parameter nu}
+#' \item{ga.pr.mean}{prior mean of parameter gamma}
+#' \item{ga.pr.sd}{prior standard deviation of parameter gamma}
+#' \item{run.min}{running time in minutes}
+#'
+#' @export
+#'
+
+stanfit.to.metastats = function(stanfit.object, stan.dat){
+  obj = stanfit.object
+
+  # seed generated within model fitting
+  mod.seed = rstan::get_seed(obj)
+
+  # stancode name
+  stancode.name = obj@model_name
+
+  # prior specifications
+  th.pr.mean = stan.dat$t_expect
+  th.pr.sd = stan.dat$t_stdev
+  nu.pr.mean = stan.dat$n_expect
+  nu.pr.sd = stan.dat$n_stdev
+  ga.pr.mean = stan.dat$g_expect
+  ga.pr.sd = stan.dat$g_stdev
+
+  run.min = sum(rstan::get_elapsed_time(obj))/60 # in minutes
+
+  metastats = data.frame(mod.seed,
+                         stancode.name,
+                         th.pr.mean,
+                         th.pr.sd,
+                         nu.pr.mean,
+                         nu.pr.sd,
+                         ga.pr.mean,
+                         ga.pr.sd,
+                         run.min)
+  return(metastats)
+}
+
+

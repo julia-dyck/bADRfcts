a65dca9e302273436ab4878d80ffc43997a0faf6
julia-dyck <julia.alexandra.dyck@gmail.com>
2022-10-28 12:30:17 +0200

    add option to use previously fitted stanfit.object to retrieve the model structure

--- R/fit.fgg_function_1kernel_version.R
+++ R/fit.fgg_function_1kernel_version.R
@@ -5,6 +5,12 @@
 #'
 #' WICHTIG: warmup (1000) und iter (6000) muss noch angepasst werden!
 #'
+#' @param datstan time-event data in list format. Can be formatted using
+#'        \code{survdat2pgwstanmodeldat} function.
+#' @param fst.fit logical: whether it is the first fit of this kind of function
+#'        (if not, the previous stan fitting outcome is used to retrieve the
+#'        model structure rather than recompiling it from the C++ code).
+#'
 #' @export
 #'
 
@@ -48,22 +54,42 @@ fit.fgg = function(datstan, fst.fit = F){
 #'
 #' WICHTIG: warmup (1000) und iter (15000) muss noch angepasst werden!
 #'
+#' @param datstan time-event data in list format. Can be formatted using
+#'        \code{survdat2pgwstanmodeldat} function.
+#' @param fst.fit logical: whether it is the first fit of this kind of function
+#'        (if not, the previous stan fitting outcome is used to retrieve the
+#'        model structure rather than recompiling it from the C++ code).
+#'
 #' @export
 #'
 
 
 
-fit.ggg = function(datstan){
-  output = rstan::stan(
-    file = "stanfiles/gam.gam.gam.stan",  # Stan program
-    model_name = "gam.gam.gam.model", # model name
-    data = datstan,    # named list of data
-    chains = 4,             # number of Markov chains
-    warmup = 100,          # number of warmup iterations per chain
-    iter = 600,            # total number of iterations per chain (including warmup)
-    cores = 1,              # number of cores
-    refresh = -1             # progress not shown
-  )
+fit.ggg = function(datstan, fst.fit = F){
+  if(fst.fit == T){
+    output = rstan::stan(
+      file = "stanfiles/gam.gam.gam.stan",  # Stan program
+      model_name = "gam.gam.gam.model", # model name
+      data = datstan,    # named list of data
+      chains = 4,             # number of Markov chains
+      warmup = 100,          # number of warmup iterations per chain
+      iter = 600,            # total number of iterations per chain (including warmup)
+      cores = 1,              # number of cores
+      refresh = -1             # progress not shown
+    )
+  }
+  else{
+    output = rstan::stan(
+      fit = fst.fit.ggg,  # Stan program
+      model_name = "gam.gam.gam.model", # model name
+      data = datstan,    # named list of data
+      chains = 4,             # number of Markov chains
+      warmup = 100,          # number of warmup iterations per chain
+      iter = 600,            # total number of iterations per chain (including warmup)
+      cores = 1,              # number of cores
+      refresh = -1             # progress not shown
+    )
+  }
   return(output)
 }
 
@@ -74,22 +100,43 @@ fit.ggg = function(datstan){
 #'
 #' WICHTIG: warmup (1000) und iter (15000) muss noch angepasst werden!
 #'
+#' @param datstan time-event data in list format. Can be formatted using
+#'        \code{survdat2pgwstanmodeldat} function.
+#' @param fst.fit logical: whether it is the first fit of this kind of function
+#'        (if not, the previous stan fitting outcome is used to retrieve the
+#'        model structure rather than recompiling it from the C++ code).
+#'
+#'
 #' @export
 #'
 
 
 
-fit.fll = function(datstan){
-  output = rstan::stan(
-    file = "stanfiles/fix.gam.gam.stan",  # Stan program
-    model_name = "fix.log.log.model", # model name
-    data = datstan,    # named list of data
-    chains = 4,             # number of Markov chains
-    warmup = 100,          # number of warmup iterations per chain
-    iter = 600,            # total number of iterations per chain (including warmup)
-    cores = 1,              # number of cores
-    refresh = -1             # progress not shown
-  )
+fit.fll = function(datstan, fst.fit = F){
+  if(fst.fit == T){
+    output = rstan::stan(
+      file = "stanfiles/fix.log.log.stan",  # Stan program
+      model_name = "fix.log.log.model", # model name
+      data = datstan,    # named list of data
+      chains = 4,             # number of Markov chains
+      warmup = 100,          # number of warmup iterations per chain
+      iter = 600,            # total number of iterations per chain (including warmup)
+      cores = 1,              # number of cores
+      refresh = -1             # progress not shown
+    )
+  }
+  else{
+    output = rstan::stan(
+      fit = fst.fit.fll,  # Stan program
+      model_name = "fix.log.log.model", # model name
+      data = datstan,    # named list of data
+      chains = 4,             # number of Markov chains
+      warmup = 100,          # number of warmup iterations per chain
+      iter = 600,            # total number of iterations per chain (including warmup)
+      cores = 1,              # number of cores
+      refresh = -1             # progress not shown
+    )
+  }
   return(output)
 }
 
@@ -100,21 +147,42 @@ fit.fll = function(datstan){
 #'
 #' WICHTIG: warmup (1000) und iter (15000) muss noch angepasst werden!
 #'
+#' @param datstan time-event data in list format. Can be formatted using
+#'        \code{survdat2pgwstanmodeldat} function.
+#' @param fst.fit logical: whether it is the first fit of this kind of function
+#'        (if not, the previous stan fitting outcome is used to retrieve the
+#'        model structure rather than recompiling it from the C++ code).
+#'
+#'
 #' @export
 #'
 
 
 
-fit.lll = function(datstan){
-  output = rstan::stan(
-    file = "stanfiles/log.log.log.stan",  # Stan program
-    model_name = "log.log.log.model", # model name
-    data = datstan,    # named list of data
-    chains = 4,             # number of Markov chains
-    warmup = 100,          # number of warmup iterations per chain
-    iter = 600,            # total number of iterations per chain (including warmup)
-    cores = 1,              # number of cores (one per chain)
-    refresh = -1             # progress not shown
-  )
+fit.lll = function(datstan), fst.fit = F){
+    if(fst.fit == T){
+      output = rstan::stan(
+        file = "stanfiles/log.log.log.stan",  # Stan program
+        model_name = "log.log.log.model", # model name
+        data = datstan,    # named list of data
+        chains = 4,             # number of Markov chains
+        warmup = 100,          # number of warmup iterations per chain
+        iter = 600,            # total number of iterations per chain (including warmup)
+        cores = 1,              # number of cores
+        refresh = -1             # progress not shown
+      )
+    }
+    else{
+      output = rstan::stan(
+        fit = fst.fit.lll,  # Stan program
+        model_name = "log.log.log.model", # model name
+        data = datstan,    # named list of data
+        chains = 4,             # number of Markov chains
+        warmup = 100,          # number of warmup iterations per chain
+        iter = 600,            # total number of iterations per chain (including warmup)
+        cores = 1,              # number of cores
+        refresh = -1             # progress not shown
+      )
+    }
   return(output)
 }

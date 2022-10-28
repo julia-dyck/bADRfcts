15a3073e792e389b6489196d21c6e748baf67a15
julia-dyck <julia.alexandra.dyck@gmail.com>
2022-10-28 14:34:48 +0200

    save one stanfit.object for each pgw model alternative (so it can be referred to in the fit argument of future stan commands)

---
+++ R/first.fits.R
@@ -0,0 +1,13 @@
+#' first fits
+#'
+#' Stanfit objects containing the first fit of this kind of function
+#' It is used in future stan model fitting procedures to retrieve the
+#' model structure instead of recompiling it from the C++ code again.
+#'
+#' The stanfit objects can be called with
+#'
+#' \code{fst.fit.fgg, fst.fit.ggg, fst.fit.fll, fst.fit.lll}.
+#'
+"first.fits"
+
+

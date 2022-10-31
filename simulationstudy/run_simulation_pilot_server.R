7f55e215691f4b824cd7fae700e74579a5f0e5d4
julia-dyck <julia.alexandra.dyck@gmail.com>
2022-10-31 15:39:15 +0100

    update server skript and needed objects

--- simulationstudy/run_simulation_pilot_server.R
+++ simulationstudy/run_simulation_pilot_server.R
@@ -4,17 +4,17 @@
 # load necessary packages ------------------------------------------------------
 
 # install.packages("rstan")
-library(rstan)
-parallel::detectCores() # how many cores are available?
-options(mc.cores = 12) # adjust potentially (e.g.
-# explicit nr for server runs)
-rstan_options(auto_write = TRUE)
-
-install.packages("devtools")
-library(devtools)
-devtools::install_github(repo = "julia-dyck/bADRfcts")
-library(bADRfcts)
-
+# library(rstan)
+# parallel::detectCores() # how many cores are available?
+# options(mc.cores = 12) # adjust potentially (e.g.
+# # explicit nr for server runs)
+# rstan_options(auto_write = TRUE)
+#
+# install.packages("devtools")
+# library(devtools)
+# devtools::install_github(repo = "julia-dyck/bADRfcts")
+# library(bADRfcts)
+load("bundle_functions_dataobjects.RData")
 
 # prep scenario parameters -----------------------------------------------------
 pc.pilot = expand.grid(N = c(500, 3000, 5000),
@@ -26,12 +26,21 @@ pc.pilot
 
 # run the simulation -----------------------------------------------------------
 
-save(testobject = 1:10, file = "/home/jdyck/ownCloud/bADR_simstudyres_pilot/test.object.RData")
+# save(testobject = 1:10, file = "/home/jdyck/ownCloud/bADR_simstudyres_pilot/test.object.RData")
 
 # apply(pc.pilot[1:2,], 1, sim.repeat.1.scenario,
 #       reps = 2,
 #       save = T,
 #       path = "/home/jdyck/ownCloud/bADR_simstudyres_pilot"
 # )
+
+parallel::mclapply(1:10,sim.1.scenario.1.time,
+                   scenario.pars = pc.pilot[1,],
+                   save = T,
+                   path = getwd(),
+                   mc.cores=4)
+
+
+
 
 

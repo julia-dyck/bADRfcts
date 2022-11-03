a0644f375a745726c02f4f15e726aaacacced0cd
julia-dyck <julia.alexandra.dyck@gmail.com>
2022-11-03 14:24:51 +0100

    reformat pc.pilot matrix, so one row will be interpreted as numeric vector

--- simulationstudy/run_simulation_pilot_server.R
+++ simulationstudy/run_simulation_pilot_server.R
@@ -9,22 +9,22 @@
 # options(mc.cores = 12) # adjust potentially (e.g.
 # # explicit nr for server runs)
 # rstan_options(auto_write = TRUE)
-#
-# install.packages("devtools")
-# library(devtools)
-# devtools::install_github(repo = "julia-dyck/bADRfcts")
-# library(bADRfcts)
+
 load("bundle_functions_dataobjects.RData")
 
 # prep scenario parameters -----------------------------------------------------
-pc.pilot = expand.grid(N = c(500, 3000, 5000),
+pc.pilot = as.matrix(expand.grid(N = c(500, 3000, 5000),
                        br = c(0.01, 0.05, 0.1),
                        adr = c(0, 1),
                        rel.sd = 0.05,
-                       c = 365)
+                       c = 365))
 pc.pilot
 
 # run the simulation -----------------------------------------------------------
+
+s = sim.repeat.1.scenario(counter = 87, scenario.pars = pc.pilot[1,], save = T,
+                          path = "/home/jdyck/ownCloud/bADR_simstudyres_pilot")
+
 
 # apply(pc.pilot[1:2,], 1, sim.repeat.1.scenario,
 #       reps = 2,
@@ -32,12 +32,12 @@ pc.pilot
 #       path = "/home/jdyck/ownCloud/bADR_simstudyres_pilot"
 # )
 
-parallel::mclapply(1:10,sim.1.scenario.1.time,
-                   scenario.pars = pc.pilot[8,],
-                   save = T,
-                   path = "/home/jdyck/ownCloud/bADR_simstudyres_pilot",
-                   mc.cores=12)
-
+# parallel::mclapply(3:10, sim.1.scenario.1.time,
+#                    scenario.pars = pc.pilot[18,],
+#                    save = T,
+#                    path = "/home/jdyck/ownCloud/bADR_simstudyres_pilot",
+#                    mc.cores=12)
+#
 
 
 
